9f9e09910f20ac78fb00358803c5a6f5129d8215
Anatoli33 <hadjiev1215@gmail.com>
2026-03-26 10:56:51 +0200

    Create simple Navigation comp

--- project/src/app/app.ts
+++ project/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from './home/home.js';
+import { Nav } from './nav/nav.js';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home],
+  imports: [RouterOutlet, Home, Nav],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
