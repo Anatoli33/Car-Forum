b3b6c483f6d709d0c18a8f80823ce77837470e08
Anatoli33 <hadjiev1215@gmail.com>
2026-03-27 15:16:42 +0200

    Not found component

--- project/src/app/app.routes.ts
+++ project/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home.js';
 import { Feed } from './feed/feed.js';
 import { Login } from './login/login.js';
+import { NotFound } from './not-found/not-found.js';
 import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     {path: '', component: Home},
     {path: 'feed', component: Feed, canActivate: [authGuard],},
     {path: 'login', component: Login},
+    {path: '**', component: NotFound},
 ];
