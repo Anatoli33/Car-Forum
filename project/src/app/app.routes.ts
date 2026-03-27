9c21df30fa2d1113a295b49ed33ca70d723da124
Anatoli33 <hadjiev1215@gmail.com>
2026-03-27 12:31:40 +0200

    Make working NavBar

--- project/src/app/app.routes.ts
+++ project/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home.js';
+import { Feed } from './feed/feed.js';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Home},
+    {path: 'feed', component: Feed},
+];
--- project/src/app/app.ts
+++ project/src/app/app.ts
@@ -1,12 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Home } from './home/home.js';
 import { Nav } from './nav/nav.js';
-import { Feed } from './feed/feed.js';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home, Nav, Feed],
+  imports: [RouterOutlet, Nav],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
