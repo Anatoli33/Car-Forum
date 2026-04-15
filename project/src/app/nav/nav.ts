5284f27379aa955f9135b8773fbd790a47f67309
Anatoli33 <hadjiev1215@gmail.com>
2026-04-15 11:04:12 +0300

    Navigation small fix.

--- project/src/app/nav/nav.ts
+++ project/src/app/nav/nav.ts
@@ -1,17 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service.js';
 
 @Component({
   selector: 'app-nav',
+  standalone: true,
   imports: [RouterModule],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
 export class Nav {
- constructor(public authService: AuthService) {}
+
+  constructor(
+    public authService: AuthService,
+    private router: Router
+  ) {}
 
   async logout() {
     await this.authService.logout();
-  } 
+    await this.router.navigate(['/'], { replaceUrl: true });
+  }
 }
